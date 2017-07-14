c20afcfea072aee79d4b421cc9b754a42b0f9c71
Sangeetha-N <sangeenatesan@gmail.com>
2017-07-14 15:56:31 +0530

    Create Integerstring.java

---
+++ Integerstring.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import  java.io.*;
+import java.lang.*;
+public class Integerstring{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    String h=br.readLine();
+    Integer r=Integer.parseInt(h);
+    System.out.println(r);
+  }
+}
