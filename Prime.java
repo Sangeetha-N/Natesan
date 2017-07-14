c002ada9993d40593c95de76ec14adf3ddbe6374
Sangeetha-N <sangeenatesan@gmail.com>
2017-07-14 12:24:41 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Prime{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    ArrayList<Integer> s=new ArrayList<>();
+    int i,j;
+    Integer w=Integer.parseInt(br.readLine());
+    Integer e=Integer.parseInt(br.readLine());
+    for( i=w;i<=e;i++){
+      for( j=2;j<i;j++){
+        if(i%j==0){
+          
+          break;
+        }
+        
+      }
+      if(i==j){
+        s.add(i);
+        
+      }
+    }
+    System.out.println(s);
+  }
+  
+}
+    
+    
+
