a7af227e0205d04563755de7014d3035473f08a6
Sangeetha-N <sangeenatesan@gmail.com>
2017-07-14 10:53:25 +0530

    Create Heello.java

---
+++ Heello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Hello{
+  
+  public static void main(String args[]) throws IOException{
+    String str="Hello";
+    for(int i=0;i<5;i++){
+      
+      System.out.println(str);
+
+       }
+       }
+       }
