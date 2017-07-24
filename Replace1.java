b8cc58eb477c5189e90a2c68155283bd8353a736
Sangeetha-N <sangeenatesan@gmail.com>
2017-07-24 15:51:45 +0530

    Create Replace1.java

---
+++ Replace1.java
@@ -0,0 +1,17 @@
+
+    import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Replace{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    String s=br.readLine();
+    StringBuilder st=new StringBuilder(s);
+    st.reverse();
+    
+    System.out.println(st);
+    
+    String sd=s.replaceAll("[aeiouAEIOU]","");
+    System.out.println(sd);
+  }
+}
